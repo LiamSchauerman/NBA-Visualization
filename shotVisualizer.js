e2725b78b1c93c6f6ce1c7b664dd5ad748024b33
Hack Reactor Students <accounts@hackreactor.com>
2014-09-20 11:08:25 -0700

    elements are created by the size of the data set rather than manually setting the size

--- shotVisualizer.js
+++ shotVisualizer.js
@@ -11,35 +11,63 @@ var court = container.append('rect')
 	.attr('class','court')
 	.attr('opacity',0);
 
-var Shot = function() {
-this.x = 0;
-this.y = 0;
-this.node = container.append('circle')
-              .attr('fill','purple')
-              .attr('r',8.7)
-              .attr('cx',this.x * 17.4)
-              .attr('cy',this.y * 17.4)
-              .attr('class','shot')
+// var Shot = function() {
+// this.x = 0;
+// this.y = 0;
+// this.node = container.append('circle')
+//               .attr('fill','purple')
+//               .attr('r',8.7)
+//               .attr('cx',this.x * 17.4)
+//               .attr('cy',this.y * 17.4)
+//               .attr('class','shot')
+// }
+
+
+// for(var i=0; i < shotsFullGame.length; i++) {
+//   new Shot();
+// }
+var update = function(data) {
+
+// DATA JOIN
+// Join new data with old elements, if any.
+var shots = container.selectAll('.shot').data(data);
+
+// UPDATE
+// Update old elements as needed.
+  shots.attr("class", "shot");
+
+// ENTER
+// Creating new elements as needed
+  shots.enter().append('circle')
+      .attr('class', 'shot')
+      .attr('cx',0)
+      .attr('cy',0)
+    .transition()
+      .duration(1000)
+      .attr('r',8.7)
+      .attr('cx',function(d) {return d.x * 17.4;})
+      .attr('cy',function(d) {if(d.y > 47) {d.y = 94 - d.y;} return courtHeight - (d.y * 17.4);})
+      .attr('fill',function(d) {
+        if(d.result === 'made') {
+          return 'blue';
+        } else {
+          return 'purple';
+        }});
+
+
+// d3.selectAll('.shot').data(data)
+//   .transition()
+//     .duration(2500)
+//     .attr('cx',function(d) {console.log(d.x); return d.x * 17.4;})
+//     .attr('cy',function(d) {console.log(d.y); if(d.y > 47) {d.y = 94 - d.y;} return courtHeight - (d.y * 17.4);})
+//     .attr('fill',function(d) {
+//       if(d.result === 'made') {
+//         return 'blue';
+//       } else {
+//         return 'purple';
+//       }});
 }
-
-
-for(var i=0; i < shotsFullGame.length; i++) {
-  new Shot();
-}
-
-
-
-d3.selectAll('.shot').data(shotsFullGame)
-  .transition()
-    .duration(2500)
-    .attr('cx',function(d) {console.log(d.x); return d.x * 17.4;})
-    .attr('cy',function(d) {console.log(d.y); if(d.y > 47) {d.y = 94 - d.y;} return courtHeight - (d.y * 17.4);})
-    .attr('fill',function(d) {
-      if(d.result === 'made') {
-        return 'blue';
-      } else {
-        return purple;
-      }});
+update(shotsFullGame)
 
 
 
