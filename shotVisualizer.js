a8e5636c1aa13286b564e177da1a38226cf78344
Hack Reactor Students <accounts@hackreactor.com>
2014-09-20 12:34:28 -0700

    tooltip implemented. circle attributes refactored

--- shotVisualizer.js
+++ shotVisualizer.js
@@ -1,15 +1,23 @@
+$(document).ready(function(){
+
+
+
 var courtWidth = 873;
 var courtHeight = 819;
 
 var container = d3.select(".something").append("svg")
-		.attr('width', courtWidth)
-		.attr('height', courtHeight);
+    .attr('width', courtWidth)
+    .attr('height', courtHeight);
 
 var court = container.append('rect')
-	.attr('width', courtWidth)
-	.attr('height', courtHeight)
-	.attr('class','court')
-	.attr('opacity',0);
+  .attr('width', courtWidth)
+  .attr('height', courtHeight)
+  .attr('class','court')
+  .attr('opacity',0);
+
+var viewByTeam = function(teamName) {
+
+}
 
 // var Shot = function() {
 // this.x = 0;
@@ -44,32 +52,53 @@ var shots = container.selectAll('.shot').data(data);
       .attr('cy',0)
     .transition()
       .duration(1000)
-      .attr('r',8.7)
-      .attr('cx',function(d) {return d.x * 17.4;})
-      .attr('cy',function(d) {if(d.y > 47) {d.y = 94 - d.y;} return courtHeight - (d.y * 17.4);})
-      .attr('fill',function(d) {
-        if(d.result === 'made') {
-          return 'blue';
-        } else {
-          return 'purple';
-        }});
+      .attr({
+        r: 8.7,
+        cx: function(d) {return d.x * 17.4;},
+        cy: function(d) {if(d.y > 47) {d.y = 94 - d.y;} return courtHeight - (d.y * 17.4);},
+        fill: function(d) { return d.result === 'made' ? 'green' : 'red'; },
+        player: function(d) { return d.player },
+        period: function(d) { return d.period },
+        minute: function(d) { return d.minute },
+        second: function(d) { return d.second },
+        assist: function(d) { return d.assist },
+        distance: function(d) { return d.distance },
+        result:  function(d) { return d.result }
+      });
 
-
-// d3.selectAll('.shot').data(data)
-//   .transition()
-//     .duration(2500)
-//     .attr('cx',function(d) {console.log(d.x); return d.x * 17.4;})
-//     .attr('cy',function(d) {console.log(d.y); if(d.y > 47) {d.y = 94 - d.y;} return courtHeight - (d.y * 17.4);})
-//     .attr('fill',function(d) {
-//       if(d.result === 'made') {
-//         return 'blue';
-//       } else {
-//         return 'purple';
-//       }});
 }
 update(shotsFullGame)
 
+$('circle').on('mouseover', function(){
+  var player = $(this).attr('player');
+  var assist = $(this).attr('assist');
+  console.log(assist);
+  console.log(assist.length);
+
+  $(this).attr('stroke','yellow');
+  $(this).attr('stroke-width', '4');
+  var tool = $('.tooltip');
+  var toolhtml = '<strong>'+player+'</strong>';
+  if(assist.length >0) {
+    toolhtml += '<br/> assisted by '+ assist;
+  }
+  tool.html(toolhtml);
+  console.log(tool);
+  var w = parseInt($('.tooltip').css('width'));
+  console.log(w);
+  $('.tooltip') // populating tooltip
+    .css('opacity', 1)
+    .css('top', '50px')
+    .css('left', '200px')
+    // .css('top', ($(this).attr('cy')+200).toString()+'px')
+    // .css('left', ($(this).attr('cx') - (w*.5)).toString()+'px')
+
+});
+
+$('circle').on('mouseleave', function(){
+  $(this).attr('stroke-width', '0');
+  $('.tooltip').css("opacity", 0);
+});
 
 
-
-
+});
