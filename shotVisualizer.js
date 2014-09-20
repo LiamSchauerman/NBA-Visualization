29677b46d09a9f50e4f11d531590c00aef00c2d0
Jason Erpenbeck <jason.erpenbeck@gmail.com>
2014-09-20 00:38:22 -0700

    Shot Chart is WORKING! Made and missed shots are color coded, too

--- shotVisualizer.js
+++ shotVisualizer.js
@@ -1,22 +1,24 @@
-var container = d3.select('body')
-	.append('svg')
-		.attr('width', 1600)
-		.attr('height', 1400);
+var courtWidth = 873;
+var courtHeight = 819;
+
+var container = d3.select(".something").append("svg")
+		.attr('width', courtWidth)
+		.attr('height', courtHeight);
 
 var court = container.append('rect')
-	.attr('width', 1455)
-	.attr('height', 1365)
-	.attr('fill','blue');
-	.attr('class','court');
+	.attr('width', courtWidth)
+	.attr('height', courtHeight)
+	.attr('class','court')
+	.attr('opacity',0);
 
 var Shot = function() {
-this.x = 10;
-this.y = 10;
+this.x = 0;
+this.y = 0;
 this.node = container.append('circle')
-              .attr('fill','blue')
-              .attr('r',14.55)
-              .attr('cx',this.x * 29.1)
-              .attr('cy',this.y * 29.1)
+              .attr('fill','purple')
+              .attr('r',8.7)
+              .attr('cx',this.x * 17.4)
+              .attr('cy',this.y * 17.4)
               .attr('class','shot')
 }
 
@@ -25,11 +27,19 @@ for(var i=0; i < shotsFullGame.length; i++) {
   new Shot();
 }
 
-d3.selectAll('shot').data(shotsFullGame)
+
+
+d3.selectAll('.shot').data(shotsFullGame)
   .transition()
     .duration(2500)
-    .attr('cx',function(d) {return d.x;})
-    .attr('cy',function(d) {return d.y;});
+    .attr('cx',function(d) {console.log(d.x); return d.x * 17.4;})
+    .attr('cy',function(d) {console.log(d.y); if(d.y > 47) {d.y = 94 - d.y;} return courtHeight - (d.y * 17.4);})
+    .attr('fill',function(d) {
+      if(d.result === 'made') {
+        return 'blue';
+      } else {
+        return purple;
+      }});
 
 
 
